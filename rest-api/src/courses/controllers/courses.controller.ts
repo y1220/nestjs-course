5b0b0fd8ff407cd6b00130f19e188e374ffcad2d
y1220 <ga.2.y1220@gmail.com>
2024-02-24 11:05:03 +0100

    feat(CourseApi): add find all get call

--- rest-api/src/courses/controllers/courses.controller.ts
+++ rest-api/src/courses/controllers/courses.controller.ts
@@ -1,10 +1,20 @@
 import { Controller, Get } from "@nestjs/common";
+import { Course } from "../../../../shared/course"; // Import the 'Course' type from the appropriate module
+import { findAllCourses } from "../../../db-data";
 
 @Controller()
 export class CoursesController {
 
-    @Get('/api/hello-world')
-    async helloWorld(): Promise<string>{ // return type is a promise of string
-        return "Hello World";
+    // @Get('/api/hello-world')
+    // async helloWorld(): Promise<string>{ // return type is a promise of string
+    //     return "Hello World";
+    // }
+
+    @Get('/api/courses')
+    async findAllCourses(): Promise<Course[]> {
+        // Return an array of courses (db-data)
+        return findAllCourses() as Course[];
+
     }
+
 }
